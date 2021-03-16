3f701bab4e76eb33de5fcf1d1bb24e2975a4cead
MateusVieira <mateus.vieira@trackcash.com.br>
2021-03-16 11:15:07 -0300

    Update - add router

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { router } from './routes';
 
 const app = express();
 
-app.use(express.json())
-app.use(router)
+app.use(express.json());
+app.use(router);
 
-export { app }
+export { app };
