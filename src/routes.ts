1ae01226c5bff5d6a56c354e638c95f66c28eca5
MateusVieira <mateus.vieira@trackcash.com.br>
2021-03-16 18:01:07 -0300

    Add - teste endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -4,5 +4,6 @@ import { createGetDataController } from './useCases/getData';
 const router = Router();
 
 router.get('/', (request, response) => createGetDataController.handle(request, response));
+router.get('/test', (request, response) => response.json({ data: 'teste' }));
 
 export { router };
