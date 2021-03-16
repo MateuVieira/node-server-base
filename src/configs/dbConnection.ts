ac3080ea708c80f990189471725a3cdb5653d97b
MateusVieira <mateus.vieira@trackcash.com.br>
2021-03-16 13:13:49 -0300

    Update - add env vars

--- src/configs/dbConnection.ts
+++ src/configs/dbConnection.ts
@@ -1,7 +1,10 @@
 import { Pool } from 'pg';
 
+const host = process.env.HOST;
+const port = process.env.PORT;
+
 export default new Pool({
   max: 20,
-  connectionString: 'postgres://postgres:admin@localhost:5432',
+  connectionString: `postgres://postgres:admin@${host}:${port}`,
   idleTimeoutMillis: 30000,
 });
