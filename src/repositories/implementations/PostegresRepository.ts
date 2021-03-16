bb60688f8cd15d704ced42dc52c87e161736639e
MateusVieira <mateus.vieira@trackcash.com.br>
2021-03-16 11:13:42 -0300

    Add - get data from db

--- src/repositories/implementations/PostegresRepository.ts
+++ src/repositories/implementations/PostegresRepository.ts
@@ -1,7 +1,20 @@
+import Pool from '../../configs/dbConnection';
 import { IDataRepository } from '../IDataRepository';
 
 export class PostegresRepository implements IDataRepository {
   async getData(): Promise<string> {
+    try {
+      const client = await Pool.connect();
 
+      const sql = 'SELECT * FROM message WHERE id = 2';
+      const { rows } = await client.query(sql);
+      const todos = rows;
+
+      client.release();
+
+      return todos[0].value;
+    } catch (error) {
+      return 'Fail';
+    }
   }
 }
