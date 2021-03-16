8aa0cb3a487544d228069e58542fe35c5edb80c2
MateusVieira <mateus.vieira@trackcash.com.br>
2021-03-16 17:22:12 -0300

    Update - configs db

--- src/configs/dbConnection.ts
+++ src/configs/dbConnection.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
-const host = process.env.HOST;
-const port = process.env.PORT || '0.0.0.0';
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT;
 
 export default new Pool({
   max: 20,
